f41623909c11dc126d44f96c34f705e337a80138
HP <hlushchenia@strateg.by>
2017-05-03 02:24:25 +0300

    Merge branch 'master' of https://github.com/testOrganizations123/crmturizm
    # Conflicts:
    #	modules/VDCustomReports/views/List.php

--- test/templates_c/vlayout/f87d2affd5fc27041b00c7fc7c97362cbf85ac88.file.salesFunnel.tpl.php
+++ test/templates_c/vlayout/f87d2affd5fc27041b00c7fc7c97362cbf85ac88.file.salesFunnel.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2017-04-29 00:23:23
+<?php /* Smarty version Smarty-3.1.7, created on 2017-05-02 19:15:42
          compiled from "C:\os\OpenServer\domains\crmturizm\includes\runtime/../../layouts/vlayout\modules\VDCustomReports\uitypes\salesFunnel.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:105185903a1cb460d69-08660081%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'f87d2affd5fc27041b00c7fc7c97362cbf85ac88' => 
     array (
       0 => 'C:\\os\\OpenServer\\domains\\crmturizm\\includes\\runtime/../../layouts/vlayout\\modules\\VDCustomReports\\uitypes\\salesFunnel.tpl',
-      1 => 1493414507,
+      1 => 1493671350,
       2 => 'file',
     ),
   ),
@@ -19,23 +19,57 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'unifunc' => 'content_5903a1cb49728',
   'variables' => 
   array (
-    'FUNNEL' => 0,
+    'FUNNELNEW' => 0,
+    'FUNNELALL' => 0,
+    'i' => 0,
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_5903a1cb49728')) {function content_5903a1cb49728($_smarty_tpl) {?><script>
-    window.funelData = <?php echo $_smarty_tpl->tpl_vars['FUNNEL']->value;?>
+    window.funnelDataNew = <?php echo $_smarty_tpl->tpl_vars['FUNNELNEW']->value;?>
+;
+    window.funnelDataAll = <?php echo $_smarty_tpl->tpl_vars['FUNNELALL']->value;?>
 ;
 </script>
 
 <style>
-    #chartDiv{
-        height: 500px;
-        width: 700px;
+    .funnelBlock{
+        width: 740px;
+        display: inline-block;
+        vertical-align: top;
+    }
+    .funnel{
+        width: 100%;
     }
 </style>
 
-<div id="chartDiv"></div>
+<div class="funnelBlock">
+    <?php  $_smarty_tpl->tpl_vars['value'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['value']->_loop = false;
+ $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable;
+ $_from = json_decode($_smarty_tpl->tpl_vars['FUNNELALL']->value); if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['value']->key => $_smarty_tpl->tpl_vars['value']->value){
+$_smarty_tpl->tpl_vars['value']->_loop = true;
+ $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['value']->key;
+?>
+            <div style="height: 700px" class="funnel" id="div_new_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+"></div>
+    <?php } ?>
+</div>
+
+<div class="funnelBlock">
+    <?php  $_smarty_tpl->tpl_vars['value'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['value']->_loop = false;
+ $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable;
+ $_from = json_decode($_smarty_tpl->tpl_vars['FUNNELNEW']->value); if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['value']->key => $_smarty_tpl->tpl_vars['value']->value){
+$_smarty_tpl->tpl_vars['value']->_loop = true;
+ $_smarty_tpl->tpl_vars['i']->value = $_smarty_tpl->tpl_vars['value']->key;
+?>
+        <div style="height: 700px" class="funnel" id="div_<?php echo $_smarty_tpl->tpl_vars['i']->value;?>
+"></div>
+    <?php } ?>
+</div>
+
+
 
 
 
