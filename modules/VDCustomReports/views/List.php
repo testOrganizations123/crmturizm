63ea972b386973a265dcac4f151cb22efce1d1f4
AndreyNegrov <negrov@strateg.by>
2017-05-14 23:19:23 +0300

    Merge branch 'master' of https://github.com/testOrganizations123/crmturizm
    # Conflicts:
    #	modules/VDCustomReports/views/List.php

--- modules/VDCustomReports/views/List.php
+++ modules/VDCustomReports/views/List.php
@@ -623,33 +623,28 @@ class VDCustomReports_List_View extends Vtiger_List_View
         $funnelArrayNew = $this->getFunnels($sqlNewFunnelReservation, $sqlNewFunnelApplication);
 
         $sqlAllFunnelApplication = "SELECT g.eventstatus,g.leadsource, g.meet FROM
-                    (SELECT s.due_date, l.leadid, s.eventstatus, s.activityid, l.leadsource, cl.meet, cl.crmid
+                    (SELECT s.due_date, l.leadid, s.eventstatus, s.activityid, l.leadsource, c1.meet, c1.crmid
                         FROM vtiger_leaddetails as l
-                        INNER JOIN vtiger_crmentity as cl 
-                            ON cl.crmid = l.leadid
+                        INNER JOIN vtiger_crmentity as c1 
+                            ON c1.crmid = l.leadid
                         INNER JOIN (
-                             select c1.meet, s1.crmid, s1.activityid, a1.eventstatus, a1.due_date 
+                             select cl.meet, s1.crmid, s1.activityid, a1.eventstatus, a1.due_date 
                              FROM vtiger_seactivityrel as s1 INNER JOIN vtiger_activity as a1 ON a1.activityid = s1.activityid 
-                             LEFT JOIN vtiger_crmentity as c1 ON c1.crmid = a1.activityid 
+                             LEFT JOIN vtiger_crmentity as cl ON cl.crmid = a1.activityid 
                              WHERE (CAST(a1.due_date AS DATE) BETWEEN ? AND ?) ORDER BY a1.activityid DESC) as s 
                             ON s.crmid = l.leadid
-                        LEFT JOIN vtiger_users as u ON u.id = cl.smownerid
+                        LEFT JOIN vtiger_users as u ON u.id = c1.smownerid
                         LEFT JOIN vtiger_office as o ON o.officeid = u.office
-                        WHERE cl.deleted = 0 " . $addQuery . "
+                        WHERE c1.deleted = 0 " . $addQuery . "
                         GROUP BY l.leadid 
                     ) as g GROUP BY g.crmid";
 
 
-        $sqlAllFunnelReservation = "SELECT p.amount-pcf.cf_1256 AS amount, p.amount AS amounta,((p.amount-pcf.cf_1256)/(p.amount)*100) as  echarge, p.sales_stage AS eventstatus,p.leadsource
-                        FROM vtiger_potential as p
-                        INNER JOIN vtiger_crmentity as c1 
-                            ON c1.crmid = p.potentialid
-                            INNER JOIN vtiger_potentialscf as pcf
-                            ON pcf.potentialid = p.potentialid
-                            LEFT JOIN vtiger_users as u ON u.id = c1.smownerid
-                            LEFT JOIN vtiger_office as o ON o.officeid = u.office
-              WHERE p.potentialtype <> 'Авиа билеты' and p.potentialtype <> 'ЖД билеты' and (CAST(pcf.cf_1225 AS DATE) BETWEEN ? AND ?)" . $addQuery . "
-            GROUP BY  c1.crmid";
+        $sqlAllFunnelReservation = "SELECT p.amount-pcf.cf_1256 AS amount , p.amount AS amounta, ((p.amount-pcf.cf_1256)/(p.amount)*100) as  echarge, p.sales_stage AS eventstatus, p.leadsource   FROM vtiger_potential as p INNER JOIN vtiger_crmentity as c1 ON c1.crmid = p.potentialid
+            inner join vtiger_potentialscf as pcf ON pcf.potentialid = p.potentialid
+            left join vtiger_office as o ON o.officeid = pcf.cf_1215
+            LEFT JOIN vtiger_users as u ON u.id = c1.smownerid
+            where c1.deleted=0 and p.potentialtype <> 'Авиа билеты' and p.potentialtype <> 'ЖД билеты' and (CAST( pcf.cf_1225 AS DATE) BETWEEN ? AND ?)  " . $addQuery;
 
 
         $funnelArrayAll = $this->getFunnels($sqlAllFunnelReservation, $sqlAllFunnelApplication);
