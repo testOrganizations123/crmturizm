c43c2806e8dbf05ccbfd5d80246d7d58f59595da
HP <hlushchenia@strateg.by>
2017-05-23 00:37:30 +0300

    Merge branch 'master' of https://github.com/testOrganizations123/crmturizm
    # Conflicts:
    #	modules/Accounting/views/List.php

--- modules/Accounting/Accounting.php
+++ modules/Accounting/Accounting.php
@@ -1,2 +1,25 @@
 <?php
-class Accounting {}
+class Accounting {
+    /**
+     * Invoked when special actions are performed on the module.
+     * @param String Module name
+     * @param String Event Type
+     */
+    function vtlib_handler($moduleName, $eventType) {
+        global $adb;
+        if($eventType == 'module.postinstall') {
+            // TODO Handle actions after this module is installed.
+        } else if($eventType == 'module.disabled') {
+            // TODO Handle actions before this module is being uninstalled.
+        } else if($eventType == 'module.preuninstall') {
+            // TODO Handle actions when this module is about to be deleted.
+        } else if($eventType == 'module.preupdate') {
+            // TODO Handle actions before this module is updated.
+        } else if($eventType == 'module.postupdate') {
+            // TODO Handle actions after this module is updated.
+        }
+    }
+    function getNonAdminAccessControlQuery(){
+        return true;
+    }
+}
